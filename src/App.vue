9905c31dec5797cf97f39d65002aa8255d1dc8b7
happyjia <33983550+happyjia@users.noreply.github.com>
2019-08-25 14:28:12 +0800

    init FIRST FOODSHOP

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <FooterGuide   v-if="$route.meta.showFooter"/>
+  </div>
+</template>
+
+<script>
+import FooterGuide from './components/FooterGuider/FooterGuide'
+export default {
+  name: 'App',
+  components: {FooterGuide}
+}
+</script>
+
+<style>
+
+</style>
